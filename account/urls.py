2ecaa48fe87b7791286d6acdf10c0a09ae78e9d8
saraey <saraeygh@gmail.com>
2024-09-28 14:40:00 +0330

    refqactor: move sign-in path to account app

--- account/urls.py
+++ account/urls.py
@@ -1,9 +1,15 @@
 from django.urls import path
+from core.views import CustomeObtainAuthToken
 
 from account.views import SignUpAPIView, UploadUsersAPIView
 
-
-urlpatterns = [
-    path("sign-up/", SignUpAPIView.as_view()),
+upload_users_urls = [
     path("upload-users/", UploadUsersAPIView.as_view()),
 ]
+
+sign_in_sign_up_urls = [
+    path("sign-up/", SignUpAPIView.as_view()),
+    path("sign-in/", CustomeObtainAuthToken.as_view()),
+]
+
+urlpatterns = upload_users_urls + sign_in_sign_up_urls
