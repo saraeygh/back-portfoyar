785816ea484f5e501643a8cea68dada66befb43c
saraey <saraeygh@gmail.com>
2024-09-30 15:29:37 +0330

    chore: some minor chore

--- account/views/password_apiview.py
+++ account/views/password_apiview.py
@@ -62,51 +62,51 @@ class PasswordAPIView(APIView):
             status=status.HTTP_400_BAD_REQUEST,
         )
 
-    def post(self, request):
-        new_username = request.data.get("username")
-        user_exists = User.objects.filter(username=new_username).exists()
-        if new_username is None or not is_valid_phone(new_username) or user_exists:
-            return Response(
-                {"message": "شماره موبایل نامعتبر"}, status=status.HTTP_400_BAD_REQUEST
-            )
+    # def post(self, request):
+    #     new_username = request.data.get("username")
+    #     user_exists = User.objects.filter(username=new_username).exists()
+    #     if new_username is None or not is_valid_phone(new_username) or user_exists:
+    #         return Response(
+    #             {"message": "شماره موبایل نامعتبر"}, status=status.HTTP_400_BAD_REQUEST
+    #         )
 
-        username = request.user.username
-        code = str(random.randint(123456, 999999))
-        code_saved_in_redis = set_dict_in_redis(
-            {"username": username, "new_username": new_username, "code": code}
-        )
-        if not code_saved_in_redis:
-            return Response(
-                {"message": "بعد از گذشت ۵ دقیقه دوباره تلاش کنید"},
-                status=status.HTTP_400_BAD_REQUEST,
-            )
+    #     username = request.user.username
+    #     code = str(random.randint(123456, 999999))
+    #     code_saved_in_redis = set_dict_in_redis(
+    #         {"username": username, "new_username": new_username, "code": code}
+    #     )
+    #     if not code_saved_in_redis:
+    #         return Response(
+    #             {"message": "بعد از گذشت ۵ دقیقه دوباره تلاش کنید"},
+    #             status=status.HTTP_400_BAD_REQUEST,
+    #         )
 
-        response = send_sms_code(new_username, code)
-        if response == "Ok":
-            return Response({"message": "کد تایید ارسال شد"}, status=status.HTTP_200_OK)
-        return Response(
-            {"message": "متاسفانه کد ارسال نشد، بعد از ۵ دقیقه دوباره تلاش کنید"},
-            status=status.HTTP_400_BAD_REQUEST,
-        )
+    #     response = send_sms_code(new_username, code)
+    #     if response == "Ok":
+    #         return Response({"message": "کد تایید ارسال شد"}, status=status.HTTP_200_OK)
+    #     return Response(
+    #         {"message": "متاسفانه کد ارسال نشد، بعد از ۵ دقیقه دوباره تلاش کنید"},
+    #         status=status.HTTP_400_BAD_REQUEST,
+    #     )
 
-    def patch(self, request):
-        sent_code = str(request.data.get("code"))
-        user: User = request.user
-        code_info = get_dict_from_redis(user.username)
-        if code_info is None:
-            return Response(
-                {"message": "کد منقضی شده است، لطفا دوباره تلاش کنید"},
-                status=status.HTTP_400_BAD_REQUEST,
-            )
-        generated_code = code_info.get("code")
-        if sent_code == generated_code:
-            new_username = code_info.get("new_username")
-            user.username = new_username
-            user.save()
-            return Response(
-                {"message": "نام کاربری به‌روزرسانی شد"}, status=status.HTTP_200_OK
-            )
+    # def patch(self, request):
+    #     sent_code = str(request.data.get("code"))
+    #     user: User = request.user
+    #     code_info = get_dict_from_redis(user.username)
+    #     if code_info is None:
+    #         return Response(
+    #             {"message": "کد منقضی شده است، لطفا دوباره تلاش کنید"},
+    #             status=status.HTTP_400_BAD_REQUEST,
+    #         )
+    #     generated_code = code_info.get("code")
+    #     if sent_code == generated_code:
+    #         new_username = code_info.get("new_username")
+    #         user.username = new_username
+    #         user.save()
+    #         return Response(
+    #             {"message": "نام کاربری به‌روزرسانی شد"}, status=status.HTTP_200_OK
+    #         )
 
-        return Response(
-            {"message": "کد ارسالی اشتباه است"}, status=status.HTTP_400_BAD_REQUEST
-        )
+    #     return Response(
+    #         {"message": "کد ارسالی اشتباه است"}, status=status.HTTP_400_BAD_REQUEST
+    #     )
