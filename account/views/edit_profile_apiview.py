61f6147606cd036785e3ba9c0b0003b9f627390e
saraey <saraeygh@gmail.com>
2024-10-02 12:08:05 +0330

    chore: change patch to put in profile edit

--- account/views/edit_profile_apiview.py
+++ account/views/edit_profile_apiview.py
@@ -37,7 +37,7 @@ class ProfileAPIView(APIView):
 
         return Response(profile, status=status.HTTP_200_OK)
 
-    def patch(self, request):
+    def put(self, request):
         response = {}
         user = request.user
 
