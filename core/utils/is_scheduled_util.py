5ba590bb091f7fa501a86bf144f2300c48ad73d0
saraey <saraeygh@gmail.com>
2024-11-09 09:36:40 +0330

    debug 3

--- core/utils/is_scheduled_util.py
+++ core/utils/is_scheduled_util.py
@@ -6,7 +6,6 @@ import jdatetime
 def is_scheduled(weekdays: list, start: int, end: int):
     tehran = timezone("Asia/Tehran")
     now = jdatetime.datetime.now(tz=tehran)
-    print("NOW: >>>>>>>>>>>>>>", now)
     weekday = now.weekday()
     hour = now.hour
     if weekday in weekdays and (start <= hour <= end):
