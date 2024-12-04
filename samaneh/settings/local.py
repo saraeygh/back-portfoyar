054101f82aba8d3a531b5645a00f6c89599b5e84
saraey <saraeygh@gmail.com>
2024-12-04 11:36:43 +0330

    feat: separate backend local & server docker composes  Changes committed: 	new file:   docker-compose.api.test.local.yml 	renamed:    docker-compose.api.test.yml -> docker-compose.api.test.server.yml 	modified:   samaneh/settings/local.py 	modified:   samaneh/settings/test.py

--- samaneh/settings/local.py
+++ samaneh/settings/local.py
@@ -1,13 +1,18 @@
+import os
 from .common import *
 
+POSTGRES_DB = os.environ.setdefault("POSTGRES_DB", "samane_jame_test")
+POSTGRES_USER = os.environ.setdefault("POSTGRES_USER", "arman")
+POSTGRES_PASSWORD = os.environ.setdefault("POSTGRES_PASSWORD", "arman")
+POSTGRES_SERVICE_NAME = os.environ.setdefault("POSTGRES_SERVICE_NAME", "localhost")
 
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",
-        "NAME": "samaneh_dev",
-        "USER": "arman",
-        "PASSWORD": "arman",
-        "HOST": "localhost",
+        "NAME": POSTGRES_DB,
+        "USER": POSTGRES_USER,
+        "PASSWORD": POSTGRES_PASSWORD,
+        "HOST": POSTGRES_SERVICE_NAME,
         "PORT": "5432",
     }
 }
