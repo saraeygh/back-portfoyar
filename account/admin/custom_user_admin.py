d82658d24dc4d08c7fd2e8a02b9380e3a336ce1d
saraey <saraeygh@gmail.com>
2024-12-16 14:51:25 +0330

    refactor: custom admin for User model

--- account/admin/custom_user_admin.py
+++ account/admin/custom_user_admin.py
@@ -1,3 +1,6 @@
+from django.contrib import admin
+
+from django.contrib.auth.models import User
 from django.contrib.auth.admin import UserAdmin
 
 
@@ -8,20 +11,28 @@ class CustomUserAdmin(UserAdmin):
         "email",
         "first_name",
         "last_name",
-        "is_superuser",
-        "is_staff",
         "is_active",
+        "is_staff",
+        "is_superuser",
+        "max_login",
+        "active_login",
+        "note",
     )
-    # list_filter = ("is_staff", "is_superuser", "is_active", "groups")
-    # search_fields = ("username", "first_name", "last_name", "email")
 
-    # list_display = ("id", "username")
+    ordering = ("-date_joined",)
+
+    @admin.display(description="حداکثر لاگین")
+    def max_login(self, obj: User):
+        return obj.profile.max_login
+
+    @admin.display(description="لاگین فعال")
+    def active_login(self, obj: User):
+        return obj.profile.active_login
+
+    @admin.display(description="توضیحات")
+    def note(self, obj: User):
+        return obj.profile.note
 
 
-#     list_display_links = ("id", "username")
-
-#     # ordering = ("-created_at",)
-
-#     search_fields = ("id", "username")
-
-#     # list_filter = ("gender", "max_login", "active_login", "note")
+admin.site.unregister(User)
+admin.site.register(User, CustomUserAdmin)
