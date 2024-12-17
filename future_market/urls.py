fc2afc24026ac4e7c84bb236467a25bb1e52d047
saraey <saraeygh@gmail.com>
2024-12-17 13:31:11 +0330

    refactor: change profit_status & strategies list apiview for future from option_market 	modified:   future_market/urls.py 	modified:   future_market/views/__init__.py 	deleted:    future_market/views/future_option_menu_apiview.py 	modified:   option_market/views/strategy_menu_options_apiview.py

--- future_market/urls.py
+++ future_market/urls.py
@@ -3,10 +3,9 @@ from django.urls import path
 from future_market.views import (
     MonthlyInterestRateAPIView,
     FutureOptionPositionsAPIView,
-    FutureProfitStatusesAPIView,
-    FutureStrategiesAPIView,
     FuturePositionsAPIView,
 )
+from option_market.views import ProfitStatusesAPIView, StrategiesAPIView
 
 future_urls = [
     path("monthly-interest-rate/", MonthlyInterestRateAPIView.as_view()),
@@ -14,10 +13,10 @@ future_urls = [
 ]
 
 option_urls = [
-    path("profit-statuses/", FutureProfitStatusesAPIView.as_view()),
+    path("profit-statuses/", ProfitStatusesAPIView.as_view()),
     path(
         "strategies/<str:risk_level>/<str:profit_status>/",
-        FutureStrategiesAPIView.as_view(),
+        StrategiesAPIView.as_view(),
     ),
     path(
         "options/<str:risk_level>/<str:strategy_key>/",
--- future_market/views/__init__.py
+++ future_market/views/__init__.py
@@ -1,7 +1,3 @@
 from .monthly_interest_rate_apiview import MonthlyInterestRateAPIView
 from .future_strategy_apiview import FuturePositionsAPIView
 from .future_option_positions_apiview import FutureOptionPositionsAPIView
-from .future_option_menu_apiview import (
-    FutureProfitStatusesAPIView,
-    FutureStrategiesAPIView,
-)
