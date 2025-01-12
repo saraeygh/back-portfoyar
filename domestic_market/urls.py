94bf10f388f04c0d6db392445c0812b6adc7b0d6
saraey <saraeygh@gmail.com>
2025-01-12 09:02:06 +0330

    refactor: add end slash again & append_slash =True

--- domestic_market/urls.py
+++ domestic_market/urls.py
@@ -21,57 +21,57 @@ from domestic_market.views import (
 
 # BY PRODUCER URLs
 by_producer_urls = [
-    path("producer/company/", GetProducerListAPIView.as_view()),
+    path("producer/company", GetProducerListAPIView.as_view()),
     path(
-        "producer/group/<int:company_id>/",
+        "producer/group/<int:company_id>",
         GetCommodityListByProducerAPIView.as_view(),
     ),
     path(
-        "producer/commodity/<int:company_id>/<int:group_id>/",
+        "producer/commodity/<int:company_id>/<int:group_id>",
         GetCommodityNameListByProducerAPIView.as_view(),
     ),
-    path("producer/price-chart/", DomesticPriceChartByProducerAPIView.as_view()),
-    path("producer/ratio-chart/", DomesticRatioChartByProducerAPIView.as_view()),
+    path("producer/price-chart", DomesticPriceChartByProducerAPIView.as_view()),
+    path("producer/ratio-chart", DomesticRatioChartByProducerAPIView.as_view()),
 ]
 
 # BY CATEGORY URLs
 by_category_urls = [
-    path("category/industry/", GetIndustryListAPIView.as_view()),
-    path("category/field/<int:industry_id>/", GetCommodityTypeListAPIView.as_view()),
-    path("category/group/<int:field_id>/", GetCommodityListAPIView.as_view()),
+    path("category/industry", GetIndustryListAPIView.as_view()),
+    path("category/field/<int:industry_id>", GetCommodityTypeListAPIView.as_view()),
+    path("category/group/<int:field_id>", GetCommodityListAPIView.as_view()),
     path(
-        "category/company/<int:group_id>/",
+        "category/company/<int:group_id>",
         GetProducerListByCommodityAPIView.as_view(),
     ),
     path(
-        "category/commodity/<int:company_id>/<int:group_id>/",
+        "category/commodity/<int:company_id>/<int:group_id>",
         GetCommodityNameListByProducerAPIView.as_view(),
     ),
-    path("category/price-chart/", DomesticPriceChartAPIView.as_view()),
-    path("category/ratio-chart/", DomesticRatioChartAPIView.as_view()),
+    path("category/price-chart", DomesticPriceChartAPIView.as_view()),
+    path("category/ratio-chart", DomesticRatioChartAPIView.as_view()),
 ]
 
 
 # STRATEGIES
 strategy_urls = [
-    path("mean-deviation/", MeanDeviationAPIView.as_view()),
+    path("mean-deviation", MeanDeviationAPIView.as_view()),
     # MEAN-DEVIATION NEW VERSION
-    path("top-price-change/", MeanDeviationAPIViewV2.as_view()),
+    path("top-price-change", MeanDeviationAPIViewV2.as_view()),
 ]
 
 # REPORTS
 report_urls = [
-    path("producer-sell/<int:company_id>/", ProducerSellReportAPIView.as_view()),
+    path("producer-sell/<int:company_id>", ProducerSellReportAPIView.as_view()),
 ]
 
 # DOLLAR
 dollar_urls = [
-    path("upload-dollar/", UploadDollarPriceAPIView.as_view()),
-    path("dollar/", GetDollarPriceAPIView.as_view()),
+    path("upload-dollar", UploadDollarPriceAPIView.as_view()),
+    path("dollar", GetDollarPriceAPIView.as_view()),
 ]
 
 relation_urls = [
-    path("upload-relation/", UploadRelationAPIView.as_view()),
+    path("upload-relation", UploadRelationAPIView.as_view()),
 ]
 
 urlpatterns = (
--- favorite/urls.py
+++ favorite/urls.py
@@ -18,59 +18,59 @@ from favorite.views import (
 domestic_favorites_urls = [
     # PRODUCER URLs
     path(
-        "domestic-price/producer/",
+        "domestic-price/producer",
         DomesticFavoritePriceChartByProducerAPIView.as_view(),
     ),
     path(
-        "domestic-price/producer/<int:favorite_id>/",
+        "domestic-price/producer/<int:favorite_id>",
         DomesticFavoritePriceChartByProducerAPIView.as_view(),
     ),
     path(
-        "domestic-ratio/producer/",
+        "domestic-ratio/producer",
         DomesticFavoriteRatioChartByProducerAPIView.as_view(),
     ),
     path(
-        "domestic-ratio/producer/<int:favorite_id>/",
+        "domestic-ratio/producer/<int:favorite_id>",
         DomesticFavoriteRatioChartByProducerAPIView.as_view(),
     ),
     # CATEGORY URLs
     path(
-        "domestic-price/category/",
+        "domestic-price/category",
         DomesticFavoritePriceChartByCategoryAPIView.as_view(),
     ),
     path(
-        "domestic-price/category/<int:favorite_id>/",
+        "domestic-price/category/<int:favorite_id>",
         DomesticFavoritePriceChartByCategoryAPIView.as_view(),
     ),
     path(
-        "domestic-ratio/category/",
+        "domestic-ratio/category",
         DomesticFavoriteRatioChartByCategoryAPIView.as_view(),
     ),
     path(
-        "domestic-ratio/category/<int:favorite_id>/",
+        "domestic-ratio/category/<int:favorite_id>",
         DomesticFavoriteRatioChartByCategoryAPIView.as_view(),
     ),
     # PRODUCER SELL URLs
     path(
-        "domestic-production/",
+        "domestic-production",
         DomesticFavoriteProducerReportAPIView.as_view(),
     ),
     path(
-        "domestic-production/<int:favorite_id>/",
+        "domestic-production/<int:favorite_id>",
         DomesticFavoriteProducerReportAPIView.as_view(),
     ),
 ]
 
 
 global_favorites_urls = [
-    path("global-favorite-price-chart/", GlobalFavoritePriceChartAPIView.as_view()),
+    path("global-favorite-price-chart", GlobalFavoritePriceChartAPIView.as_view()),
     path(
-        "global-favorite-price-chart/<int:favorite_price_chart_id>/",
+        "global-favorite-price-chart/<int:favorite_price_chart_id>",
         GlobalFavoritePriceChartAPIView.as_view(),
     ),
-    path("global-favorite-ratio-chart/", GlobalFavoriteRatioChartAPIView.as_view()),
+    path("global-favorite-ratio-chart", GlobalFavoriteRatioChartAPIView.as_view()),
     path(
-        "global-favorite-ratio-chart/<int:favorite_ratio_chart_id>/",
+        "global-favorite-ratio-chart/<int:favorite_ratio_chart_id>",
         GlobalFavoriteRatioChartAPIView.as_view(),
     ),
 ]
@@ -87,20 +87,20 @@ options_favorites_urls = [
 ]
 
 stock_favorite_urls = [
-    path("favorite-roi-groups/", StockFavoriteROIGroupAPIView.as_view()),
+    path("favorite-roi-groups", StockFavoriteROIGroupAPIView.as_view()),
     path(
-        "favorite-roi-groups/<int:favorite_id>/", StockFavoriteROIGroupAPIView.as_view()
+        "favorite-roi-groups/<int:favorite_id>", StockFavoriteROIGroupAPIView.as_view()
     ),
     path(
-        "favorite-roi-instruments/",
+        "favorite-roi-instruments",
         StockFavoriteROIInstrumentListAPIView.as_view(),
     ),
     path(
-        "favorite-roi-instruments/<int:group_id>/",
+        "favorite-roi-instruments/<int:group_id>",
         StockFavoriteROIInstrumentAPIView.as_view(),
     ),
     path(
-        "favorite-roi-instruments/<int:group_id>/<str:symbol>/",
+        "favorite-roi-instruments/<int:group_id>/<str:symbol>",
         StockFavoriteROIInstrumentAPIView.as_view(),
     ),
 ]
