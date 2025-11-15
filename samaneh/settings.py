d95100288d726d12df1ecdf419ba9a61d87a39c5
Saraey <saraey.gholamreza@gmail.com>
2025-11-15 14:43:18 +0330

    chore

--- samaneh/settings.py
+++ samaneh/settings.py
@@ -154,7 +154,7 @@ TIME_ZONE = "UTC"
 USE_I18N = True
 USE_TZ = True
 
-STATIC_URL = "/static/"
+STATIC_URL = "/django-static/"
 STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
 MEDIA_URL = "/media/"
